0a194c1fe6526b9ccf255792600291a45c4a4fc8
root <root@localhost.localdomain>
2021-04-27 14:21:43 +0800

    first commit

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+import JSBI from 'jsbi'
+
+export const FACTORY_ADDRESS = '0x8D34e43AeB69e15DE1Fd5ad666237619C1210251'
+
+export const INIT_CODE_HASH = '0xef378a4c6d86cacf0a61d8f965ac4063d4e321fdb025c302ca5d13ae6e12c273'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const FIVE = JSBI.BigInt(5)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
